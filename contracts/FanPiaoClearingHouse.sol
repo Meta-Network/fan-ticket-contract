e3ce7cad00dc42f5e30e22ca36f09aa497b97452
Frank Wei <frank@frankwei.xyz>
2021-07-20 15:53:14 +0800

    feat: the ClearingHouse v1

---
+++ contracts/FanPiaoClearingHouse.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { ERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+
+contract FanPiaoClearingHouse {
+    struct TransferOrder {
+        address token;
+        address from;
+        address to;
+        uint value;
+        uint deadline;
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+    }
+
+    function handleTransferOrders(TransferOrder[] calldata orders) external {
+        for (uint256 i = 0; i < orders.length; i++) {
+            TransferOrder calldata order = orders[i];
+            // Tip: permit `spender` is the ClearingHouse contract
+            // because the allowance is based on the `transferFrom` caller, which is this contract.
+            ERC20Permit(order.token).permit(order.from, address(this), order.value, order.deadline, order.v, order.r, order.s);
+            IERC20(order.token).transferFrom(order.from, order.to, order.value);
+        }
+    }
+}
---
+++ test/ClearingHouse.ts
@@ -0,0 +1,109 @@
+import chai from "chai";
+import { solidity } from "ethereum-waffle";
+import { ethers } from "hardhat";
+import { Signer, utils, providers, BigNumberish } from "ethers";
+import type { FanPiaoV2 } from "../typechain/FanPiaoV2";
+import type { FanPiaoClearingHouse } from "../typechain/FanPiaoClearingHouse";
+import { BigNumber } from "ethers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+chai.use(solidity);
+
+const getDeadline = (howManySecond = 3600) => Math.floor(Date.now() / 1000) + howManySecond;
+
+type TransferOrder = {
+  token: string;
+  from: string;
+  to: string;
+  value: BigNumberish;
+  deadline: BigNumberish;
+  v: BigNumberish;
+  r: string;
+  s: string;
+};
+
+describe("Clearing House", function () {
+  let accounts: SignerWithAddress[];
+  let minter: SignerWithAddress;
+  let fanpiaoA: FanPiaoV2;
+  let fanpiaoB: FanPiaoV2;
+  let clearingHouse: FanPiaoClearingHouse;
+
+  beforeEach(async function () {
+    accounts = await ethers.getSigners();
+    minter = accounts[0];
+    const FanPiaoV2 = await ethers.getContractFactory("FanPiaoV2");
+    const ClearingHouse = await ethers.getContractFactory("FanPiaoClearingHouse");
+    const fa = await FanPiaoV2.deploy("Test FanPiao A", "TFPA", minter.address);
+    const fb = await FanPiaoV2.deploy("Test FanPiao A", "TFPB", minter.address);
+
+    fanpiaoA = (await fa.deployed()) as FanPiaoV2;
+    fanpiaoB = (await fb.deployed()) as FanPiaoV2;
+    clearingHouse = await (await ClearingHouse.deploy()).deployed() as FanPiaoClearingHouse
+    await fanpiaoA.mint(accounts[1].address, "1000000000000000000000000000000000000000000")
+    await fanpiaoB.mint(accounts[2].address, "1000000000000000000000000000000000000000000")
+  });
+
+  it("transfer with permits", async function () {
+    const [ _, fAOwner, fBOwner, ...rest ] = accounts;
+
+    
+    const ordersOfTokenA: TransferOrder[] = await Promise.all(rest.map((i, idx) => TransferOrderConstuctor(fanpiaoA, clearingHouse, fAOwner, i.address, "1000000", idx)));
+    const ordersOfTokenB: TransferOrder[] = await Promise.all(rest.map((i, idx) => TransferOrderConstuctor(fanpiaoB, clearingHouse, fBOwner, i.address, "2000000", idx)));
+    const orders: TransferOrder[] = [...ordersOfTokenA, ...ordersOfTokenB];
+    const estGas = await clearingHouse.estimateGas.handleTransferOrders(orders)
+    console.info(`estimated Gas cost for ${orders.length} transfers: `, estGas.toString())
+    await chai.expect(
+      clearingHouse.handleTransferOrders(orders)
+    ).to.be.not.reverted;
+    chai.expect(await fanpiaoA.balanceOf(accounts[5].address)).to.be.eq(1000000)
+    chai.expect(await fanpiaoB.balanceOf(accounts[8].address)).to.be.eq(2000000)
+  });
+});
+
+
+async function TransferOrderConstuctor(token: FanPiaoV2, clearingHouse: FanPiaoClearingHouse, from: SignerWithAddress, to: string, value: BigNumberish, nonce: number): Promise<TransferOrder> {
+  const deadline = getDeadline()
+  const chainId = await from.getChainId();
+
+  const signature = await (from as unknown as providers.JsonRpcSigner)._signTypedData(
+    {
+      name: (await token.name()),
+      version: "1",
+      chainId: chainId,
+      verifyingContract: token.address,
+    },
+    {
+      Permit: [
+        { name: "owner", type: "address" },
+        { name: "spender", type: "address" },
+        {
+          name: "value",
+          type: "uint256",
+        },
+        { name: "nonce", type: "uint256" },
+        { name: "deadline", type: "uint256" },
+      ],
+    },
+    {
+      owner: from.address,
+      // the spender is the ClearingHouse who make the `transferFrom`
+      spender: clearingHouse.address,
+      value,
+      nonce,
+      deadline
+    }
+  );
+
+  const { r, s, v } = utils.splitSignature(signature);
+
+  return {
+    token: token.address,
+    from: from.address,
+    to,
+    value,
+    deadline,
+    v,
+    r,
+    s
+  }
+}
