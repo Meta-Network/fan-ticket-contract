5600a32863f51eeb3bc2372dca38bf09b8a8351b
Frank Wei <frank@frankwei.xyz>
2021-07-26 15:14:36 +0800

    go

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,8 @@ import { task, HardhatUserConfig } from "hardhat/config";
 // import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-ethers";
 import "@openzeppelin/hardhat-upgrades";
+require('dotenv').config();
+import "@nomiclabs/hardhat-etherscan";
 import "@typechain/hardhat";
 
 // // This is a sample Hardhat task. To learn how to create your own go to
@@ -16,13 +18,32 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
-const config: HardhatUserConfig = { solidity: {
-  version: "0.8.4",
-  settings: {
-    optimizer: {
-      enabled: true,
-      runs: 99999
-    }
-  }
-} };
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 99999,
+      },
+    },
+  },
+  networks: {
+    rinkeby: {
+      chainId: 4,
+      url: "https://eth-rinkeby.alchemyapi.io/v2/" + process.env.ALCHEMY_KEY,
+      timeout: 1000 * 60,
+    },
+    bsc_testnet: {
+      chainId: 97,
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      timeout: 1000 * 60,
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.BSC_SCAN_KEY,
+  },
+};
 export default config;
