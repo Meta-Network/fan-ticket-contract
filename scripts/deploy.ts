b565c1ad1bf7d35b7d992c36636bf40d24eeef55
Frank Wei <frank@frankwei.xyz>
2021-07-26 15:14:26 +0800

    feat: deploy script

---
+++ scripts/deploy.ts
@@ -0,0 +1,84 @@
+import { promises as fs } from 'fs';
+import { JsonRpcProvider } from '@ethersproject/providers';
+import { Wallet } from '@ethersproject/wallet';
+import { MetaNetworkRoleRegistry__factory } from '../typechain/factories/MetaNetworkRoleRegistry__factory';
+import { FanTicketFactory__factory } from '../typechain/factories/FanTicketFactory__factory';
+import { FanTicketClearingHouse__factory } from '../typechain/factories/FanTicketClearingHouse__factory';
+import { utils } from 'ethers';
+import { config as loadEnvConfig } from "dotenv";
+
+// require('dotenv').config({})
+
+type AddressBook = {
+  Registry?: string;
+  Factory?: string;
+  ClearingHouse?: string;
+}
+
+async function start() {
+  const args = require('minimist')(process.argv.slice(2));
+
+  if (!args.chainId) {
+    throw new Error('--chainId chain ID is required');
+  }
+  const loadEnvFile = `.env.${args.chainId}`;
+  console.info(`Loading local env file: ${loadEnvFile}`)
+  loadEnvConfig({
+    path: loadEnvFile
+  })
+  const gasPrice = utils.parseUnits(args.gasPrice || '20', 'gwei');
+
+  const provider = new JsonRpcProvider(process.env.RPC_ENDPOINT);
+  const wallet = new Wallet(`0x${process.env.PRIVATE_KEY}`, provider);
+  const sharedAddressPath = `${process.cwd()}/addresses/${args.chainId}.json`;
+  // @ts-ignore
+  const addressBook = JSON.parse(await fs.readFile(sharedAddressPath)) as AddressBook;
+  if (addressBook.ClearingHouse) {
+    throw new Error(
+      `ClearingHouse already exists in address book at ${sharedAddressPath}. Please move it first so it is not overwritten`
+    );
+  }
+  if (addressBook.Factory) {
+    throw new Error(
+      `Factory already exists in address book at ${sharedAddressPath}. Please move it first so it is not overwritten`
+    );
+  }
+  if (addressBook.Registry) {
+    throw new Error(
+      `Registry already exists in address book at ${sharedAddressPath}. Please move it first so it is not overwritten`
+    );
+  }
+
+  console.log('Deploying MetaNetworkRoleRegistry...');
+  const deployTx = await new MetaNetworkRoleRegistry__factory(wallet).deploy({ gasPrice });
+  console.log('Deploy TX: ', deployTx.deployTransaction.hash);
+  await deployTx.deployed();
+  console.log('MetaNetworkRoleRegistry deployed at ', deployTx.address);
+  addressBook.Registry = deployTx.address;
+
+  console.log('Deploying FanTicket Factory...');
+  const factoryDeployTx = await new FanTicketFactory__factory(
+    wallet
+  ).deploy(addressBook.Registry, { gasPrice });
+  console.log(`Deploy TX: ${factoryDeployTx.deployTransaction.hash}`);
+  await factoryDeployTx.deployed();
+  console.log(`Factory deployed at ${factoryDeployTx.address}`);
+  addressBook.Factory = factoryDeployTx.address;
+
+  console.log('Deploying Clearing House...');
+  const clearingDeployTx = await new FanTicketClearingHouse__factory(
+    wallet
+  ).deploy({ gasPrice });
+  console.log(`Deploy TX: ${clearingDeployTx.deployTransaction.hash}`);
+  await clearingDeployTx.deployed();
+  console.log(`Factory deployed at ${clearingDeployTx.address}`);
+  addressBook.ClearingHouse = clearingDeployTx.address;
+
+  await fs.writeFile(sharedAddressPath, JSON.stringify(addressBook, null, 2));
+  console.log(`Contracts deployed and configured. ☼☽`);
+}
+
+start().catch((e: Error) => {
+  console.error(e);
+  process.exit(1);
+});
