b26d00444944407ae68b5a87ad8ffe19f256661b
Frank Wei <frank@frankwei.xyz>
2021-07-28 18:01:30 +0800

    feat: support 2612 `permit`

--- contracts/FanTicketClearingHouse.sol
+++ contracts/FanTicketClearingHouse.sol
@@ -27,13 +27,14 @@ interface IERC20Sig {
 
 
 contract FanTicketClearingHouse {
+    enum TxType { Transfer, Mint, Permit }
     struct TransferOrder {
         address token;
         address from;
         address to;
         uint value;
         uint deadline;
-        bool isMint;
+        TxType _type;
         uint8 v;
         bytes32 r;
         bytes32 s;
@@ -42,11 +43,14 @@ contract FanTicketClearingHouse {
     function handleTransferOrders(TransferOrder[] calldata orders) external {
         for (uint256 i = 0; i < orders.length; i++) {
             TransferOrder calldata order = orders[i];
-            if (order.isMint) {
+            if (order._type == TxType.Mint) {
                 IERC20Sig(order.token).mintBySig(order.from, order.to, order.value, order.deadline, order.v, order.r, order.s);
             }
-            else {
+            else if (order._type == TxType.Transfer) {
                 IERC20Sig(order.token).transferFromBySig(order.from, order.to, order.value, order.deadline, order.v, order.r, order.s);
+            } else {
+                // It's `permit()` then.
+                ERC20Permit(order.token).permit(order.from, order.to, order.value, order.deadline, order.v, order.r, order.s);
             }
         }
     }
